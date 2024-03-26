727ce9fb252701b24f2d91bca8193a8426348dbb
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-03-26 17:16:02 +0100

    fix bug

--- gameTable.py
+++ gameTable.py
@@ -1,15 +1,14 @@
 
 
 class gameTable():
-    def __init__(self):
+    def __init__(self) -> None:
         self.table = [[0 for i in range(7)] for j in range(7)]
 
     def __str__(self) -> str:
         s = ''
-        for i in range(7):
-            s += str(self.table) + '\n'
+        for row in self.table:
+            s += str(row) + '\n'
         return s
     
     def getElement(self, i: int, j: int) -> int:
         return self.table[i][j]
-        
